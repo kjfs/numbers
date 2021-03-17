8bbffb7f96f365d54d4231ec805eec9cefbbffef
Kjfs <karl.studnik@gmail.com>
2021-03-17 18:37:32 +0100

    1st commit

---
+++ callback.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	xi := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	t := evenSum(sum, xi...)
+	fmt.Println(t)
+}
+
+func sum(x ...int) int {
+	n := 0
+	for _, v := range x {
+		n += v
+	}
+	return n
+}
+
+func evenSum(f func(x ...int) int, y ...int) int {
+	var xi []int
+	for _, v := range y {
+		if v%2 == 0 {
+			xi = append(xi, v)
+		}
+	}
+	total := f(xi...)
+	return total
+}
